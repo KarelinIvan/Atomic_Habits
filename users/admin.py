8969ad6c4931521b116730d2d7f6ff949744ca60
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-12-08 17:59:20 +0300

    7-й коммит

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Понель пользователи  в админке"""
+    list_display = ('email', 'id', 'phone')
